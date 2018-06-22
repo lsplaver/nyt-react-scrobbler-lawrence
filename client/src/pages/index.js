afc563b473dbed803d5e018c9edbbc560a0b1f52
lsplaver <lsplaver@yahoo.com>
2018-06-22 00:21:47 -0400

    Updated pages/index for preferred format

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,9 @@
-import Home from "./Home.js";
-import Saved from "./Saved.js";
+// import Home from "./Home.js";
+// import Saved from "./Saved.js";
 
-module.exports = Home;
+// module.exports = Home;
 
-module.exports = Saved;
+// module.exports = Saved;
+
+export * from "./Home";
+export * from "./Saved";
